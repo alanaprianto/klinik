e3074d81b49554e091db0f3f0b01ac941befa36b
Alan Aprianto <alan.aprianto@gmail.com>
2017-04-20 13:54:48 +0700

    refactoring

--- app/Http/Controllers/ApiCommonController.php
+++ app/Http/Controllers/ApiCommonController.php
@@ -6,13 +6,13 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class CommonController extends GeneralController
+class ApiCommonController extends GeneralController
 {
     public function info()
     {
         $response = [];
         try {
-            $user = User::with(['staff', 'roles'])->find(Auth::user()->id);
+            $user = User::with(['staff', 'staff.staffJob', 'staff.staffPosition' ,'roles'])->find(Auth::user()->id);
             $response = ['isSuccess' => true, 'message' => 'Success / Berhasil', 'datas' => ['user' => $user]];
         } catch (\Exception $e) {
             $response = ['isSuccess' => false, 'message' => $e->getMessage(), 'datas' => null, 'code' => $e->getCode()];
