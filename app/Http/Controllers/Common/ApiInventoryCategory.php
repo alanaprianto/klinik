7938fab5669d5b1bb275dd8fcfbe3ad0286ab861
Alan Aprianto <alan.aprianto@gmail.com>
2017-05-10 13:47:34 +0700

    inventory category

---
+++ app/Http/Controllers/Common/ApiInventoryCategory.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace App\Http\Controllers\Common;
+
+use App\InventoryCategory;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ApiInventoryCategory extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $response = [];
+        try {
+            $inventory_categories = InventoryCategory::get();
+            $response = ['isSuccess' => true, 'message' => 'Success / Berhasil', 'datas' => ['inventory_categories' => $inventory_categories, 'recordsTotal' => count($inventory_categories)]];
+        } catch (\Exception $e) {
+            $response = ['isSuccess' => false, 'message' => $e->getMessage(), 'datas' => null, 'code' => $e->getCode()];
+        }
+        return response()->json($response);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $response = [];
+        try {
+            $input = $request->all();
+            $inventory_category = '';
+            if (isset($input['inventory_category_id']) && $input['inventory_category_id']) {
+                $inventory_category = InventoryCategory::find($input['inventory_category_id']);
+                $inventory_category->update($input);
+            } else {
+                $inventory_category = InventoryCategory::create($input);
+            }
+            $response = ['isSuccess' => true, 'message' => 'Success / Berhasil', 'datas' => ['inventory_category' => $inventory_category]];
+        } catch (\Exception $e) {
+            $response = ['isSuccess' => false, 'message' => $e->getMessage(), 'datas' => null, 'code' => $e->getCode()];
+        }
+        return response()->json($response);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $response = [];
+        try {
+            $inventory_category = InventoryCategory::find($id);
+            $response = ['isSuccess' => true, 'message' => 'Success / Berhasil', 'datas' => ['inventory_category' => $inventory_category]];
+        } catch (\Exception $e) {
+            $response = ['isSuccess' => false, 'message' => $e->getMessage(), 'datas' => null, 'code' => $e->getCode()];
+        }
+        return response()->json($response);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $response = [];
+        try {
+            $inventory_category = InventoryCategory::find($id);
+            $response = ['isSuccess' => true, 'message' => 'Success / Berhasil', 'datas' => ['inventory_category' => $inventory_category]];
+        } catch (\Exception $e) {
+            $response = ['isSuccess' => false, 'message' => $e->getMessage(), 'datas' => null, 'code' => $e->getCode()];
+        }
+        return response()->json($response);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $response = [];
+        try {
+            $inventory_category = InventoryCategory::find($id);
+            $inventory_category->delete();
+            $response = ['isSuccess' => true, 'message' => 'Success / Berhasil', 'datas' => []];
+        } catch (\Exception $e) {
+            $response = ['isSuccess' => false, 'message' => $e->getMessage(), 'datas' => null, 'code' => $e->getCode()];
+        }
+        return response()->json($response);
+    }
+}
